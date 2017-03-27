4e1a6c08f50a78d49fb06395af39c026ca235f8f
Thibault Falque <thibault_falque@ens.univ-artois.fr>
2017-03-27 15:52:20 +0200

    src

---
+++ src/fr/univartois/sonargo/SonarGoPlugin.java
@@ -0,0 +1,12 @@
+package fr.univartois.sonargo;
+
+import org.sonar.api.Plugin;
+
+public class SonarGoPlugin implements Plugin{
+
+	public void define(Context arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
