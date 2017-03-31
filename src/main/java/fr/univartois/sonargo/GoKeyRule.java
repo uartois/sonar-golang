6e0cda38fb8f7715c5c544613752da0b4514adef
Thibault Falque <thibault_falque@ens.univ-artois.fr>
2017-03-31 17:05:55 +0200

    création de la classe permettant de lire un fichier de propriété afin de faire la correspondance entre le fichier rège sonar et le fichier de rapport gometalinter

---
+++ src/main/java/fr/univartois/sonargo/GoKeyRule.java
@@ -0,0 +1,42 @@
+package fr.univartois.sonargo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
+
+public class GoKeyRule{
+	public static final String PATH_FILE="config/key.properties";
+	private static final Logger LOGGER=Loggers.get(GoKeyRule.class);
+	private static Properties prop=new Properties();
+	private static void init() throws FileNotFoundException, IOException{
+		prop.load(new FileInputStream(new File(PATH_FILE)));
+	}
+	
+	public static String getKeyFromDescription(String description) throws IllegalArgumentException{
+		try {
+			init();
+			Pattern pattern;
+			Matcher matcher;
+			for(Entry<Object, Object> e : prop.entrySet()) {
+	           pattern=Pattern.compile((String) e.getValue());
+	           matcher=pattern.matcher(description);
+	           if(!matcher.find()) continue;
+	           return (String) e.getKey();
+	        }
+			
+			throw new IllegalArgumentException("This description \""+description+"\" is not usable");
+			
+		} catch (IOException e) {
+			LOGGER.error("Unable to load the config file", e);
+		}
+		return null;
+	}
+}
