7ade1e8622979b11530385391667afff232bddd5
Thibault Falque <thibault_falque@ens.univ-artois.fr>
2017-04-04 13:28:43 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-mvn package org.codehaus.sonar:sonar-dev-maven-plugin::upload -DsonarHome=$SONAR_HOME -e -X
+mvn clean install
+cp target/sonar-golang-plugin-1.0.jar /home/thibault/sonarqube-6.3/extensions/plugins/sonar-golang-plugin-1.0.jar
+
