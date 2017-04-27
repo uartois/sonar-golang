a5f3e8a9a066d5aaeb738973450d519fba811810
Thibault Falque <thibault_falque@ens.univ-artois.fr>
2017-04-27 14:01:06 +0200

    format

--- src/main/java/fr/univartois/sonargo/GoLanguage.java
+++ src/main/java/fr/univartois/sonargo/GoLanguage.java
@@ -21,50 +21,52 @@
  *******************************************************************************/
 package fr.univartois.sonargo;
 
-import org.sonar.api.internal.apachecommons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.sonar.api.resources.AbstractLanguage;
 
 /**
  * This class defines the Golang language
+ * 
  * @author thibault
  *
  */
-public class GoLanguage extends AbstractLanguage{
-	
-	
-	public static final String NAME="GO";
-	public static final String KEY="go";
+public class GoLanguage extends AbstractLanguage {
 
-	private static final String[] DEFAULT_FILE_SUFFIXES={"go"};
-	
+	public static final String NAME = "GO";
+	public static final String KEY = "go";
+
+	private static final String[] DEFAULT_FILE_SUFFIXES = { "go" };
+
 	/**
-	 * Create the GoLangage 
+	 * Create the GoLangage
 	 */
 	public GoLanguage() {
-	    super(KEY, NAME);
+		super(KEY, NAME);
 	}
 
-	
 	/**
 	 * {@inheritDoc}}
 	 */
+	@Override
 	public String[] getFileSuffixes() {
 		return DEFAULT_FILE_SUFFIXES;
 	}
+
 	/**
 	 * Allows to know if the given file name has a valid suffix
-	 * @param filename String representing the file name
-	 * @return boolean <code>true</code> if the file name's suffix is known, <code>false</code> any other way
+	 * 
+	 * @param filename
+	 *            String representing the file name
+	 * @return boolean <code>true</code> if the file name's suffix is known,
+	 *         <code>false</code> any other way
 	 */
-	public boolean hasValidSuffixes(String filename){
-		for(String str:DEFAULT_FILE_SUFFIXES){
-			if(StringUtils.lowerCase(filename).endsWith("."+str)){
+	public boolean hasValidSuffixes(String filename) {
+		for (String str : DEFAULT_FILE_SUFFIXES) {
+			if (StringUtils.lowerCase(filename).endsWith("." + str)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	
 
 }
