6886588b07246d663811b43675c5d933b03222aa
Thibault Falque <thibault_falque@ens.univ-artois.fr>
2017-05-17 11:42:05 +0200

    toolkit package

---
+++ src/main/java/fr/univartois/sonargo/toolkit/GoColorizer.java
@@ -0,0 +1,25 @@
+package fr.univartois.sonargo.toolkit;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.sonar.colorizer.CDocTokenizer;
+import org.sonar.colorizer.CppDocTokenizer;
+import org.sonar.colorizer.KeywordsTokenizer;
+import org.sonar.colorizer.Tokenizer;
+
+import fr.univartois.sonargo.core.language.GoLexer;
+import fr.univartois.sonargo.core.language.GoLexer.Keyword;
+
+public final class GoColorizer {
+
+    private GoColorizer() {
+    }
+
+    public static List<Tokenizer> getTokenizers() {
+	return Arrays.asList(new CDocTokenizer("<span class=\"cd\">", "</span>"),
+		new CppDocTokenizer("<span class=\"cppd\">", "</span>"),
+		new KeywordsTokenizer("<span class=\"k\">", "</span>", GoLexer.Keyword.keywordValues()));
+    }
+
+}
---
+++ src/main/java/fr/univartois/sonargo/toolkit/GoToolkit.java
@@ -0,0 +1,70 @@
+package fr.univartois.sonargo.toolkit;
+
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.List;
+
+import org.sonar.api.internal.google.common.collect.ImmutableList;
+import org.sonar.colorizer.Tokenizer;
+import org.sonar.sslr.toolkit.AbstractConfigurationModel;
+import org.sonar.sslr.toolkit.ConfigurationProperty;
+import org.sonar.sslr.toolkit.Toolkit;
+
+import com.sonar.sslr.impl.Parser;
+
+import fr.univartois.sonargo.core.language.GoParser;
+
+public final class GoToolkit {
+
+    private GoToolkit() {
+    }
+
+    public static void main(String[] args) {
+	final Toolkit toolkit = new Toolkit("SonarSource : Go : Toolkit", new GoConfigurationModel());
+	toolkit.run();
+    }
+
+    static class GoConfigurationModel extends AbstractConfigurationModel {
+
+	private final ConfigurationProperty charsetProperty = new ConfigurationProperty("Charset",
+		"Charset used when opening files.", "UTF-8", newValueCandidate -> {
+		    try {
+			Charset.forName(newValueCandidate);
+			return "";
+		    } catch (final IllegalCharsetNameException e1) {
+			return "Illegal charset name: " + newValueCandidate;
+		    } catch (final UnsupportedCharsetException e2) {
+			return "Unsupported charset: " + newValueCandidate;
+		    }
+		});
+
+	@Override
+	public List<ConfigurationProperty> getProperties() {
+	    return ImmutableList.of(charsetProperty);
+	}
+
+	@Override
+	public Charset getCharset() {
+	    return Charset.forName(charsetProperty.getValue());
+	}
+
+	@Override
+	public Parser doGetParser() {
+	    updateConfiguration();
+	    return GoParser.create();
+	}
+
+	@Override
+	public List<Tokenizer> doGetTokenizers() {
+	    updateConfiguration();
+	    return GoColorizer.getTokenizers();
+	}
+
+	private static void updateConfiguration() {
+	    /* Construct a parser configuration object from the properties */
+	}
+
+    }
+
+}
