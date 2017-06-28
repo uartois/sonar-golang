c4b0921d7e1b47446fb3b03ade0da107597c94d3
thibault_falque <thibault_falque@ens.univ-artois.fr>
2017-06-28 12:02:39 +0200

    v1.1.9-beta for better correction #8

--- src/main/java/fr/univartois/sonargo/gotest/FunctionFinder.java
+++ src/main/java/fr/univartois/sonargo/gotest/FunctionFinder.java
@@ -12,49 +12,58 @@ import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 
 public class FunctionFinder {
-    private static final Logger LOGGER = Loggers.get(FunctionFinder.class);
+	private static final Logger LOGGER = Loggers.get(FunctionFinder.class);
 
-    private HashMap<String, String> result = new HashMap<>();
-    private String baseDir;
-    private Stream<Path> paths;
+	private HashMap<String, String> result = new HashMap<>();
+	private String baseDir;
+	private Stream<Path> paths;
 
-    public FunctionFinder(SensorContext context) throws IOException {
-	this.baseDir = context.fileSystem().baseDir().getPath();
-	paths = Files.walk(Paths.get(baseDir)).filter(p -> p.toFile().getName().endsWith("_test.go"));
-    }
-
-    public HashMap<String, String> searchFunction() {
-
-	paths.forEach((p) -> {
-	    LOGGER.info("search test function in " + p.toFile().getAbsolutePath());
-	    searchFunctionInFile(p);
-	});
-	LOGGER.debug(result.toString());
-	return result;
-    }
-
-    private void searchFunctionInFile(Path p) {
-
-	try (Stream<String> stream = Files.lines(p)) {
-
-	    stream.forEach((s) -> {
-		String nameFunction = searchInLine(s);
-		if (nameFunction == null)
-		    return;
-		result.put(nameFunction, p.toFile().getAbsolutePath());
-	    });
-
-	} catch (IOException e) {
-	    e.printStackTrace();
+	public FunctionFinder(SensorContext context) throws IOException {
+		this.baseDir = context.fileSystem().baseDir().getPath();
+		paths = Files.walk(Paths.get(baseDir)).filter(p -> p.toFile().getName().endsWith("_test.go"));
 	}
-    }
 
-    public String searchInLine(String s) {
-	int indexTesting = s.indexOf("Test");
-	if (indexTesting == -1)
-	    return null;
-	int indexParan = s.indexOf("(", indexTesting);
-	return s.substring(indexTesting, indexParan);
-    }
+	public HashMap<String, String> searchFunction() {
+
+		paths.forEach((p) -> {
+			LOGGER.info("search test function in " + p.toFile().getAbsolutePath());
+			searchFunctionInFile(p);
+		});
+		LOGGER.debug(result.toString());
+		return result;
+	}
+
+	private void searchFunctionInFile(Path p) {
+
+		try (Stream<String> stream = Files.lines(p)) {
+
+			stream.forEach((s) -> {
+				String nameFunction = searchInLine(s);
+				if (nameFunction == null)
+					return;
+				result.put(nameFunction, p.toFile().getAbsolutePath());
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String searchInLine(String s) {
+		int indexTesting = s.indexOf("Test");
+		int indexBrace = s.indexOf('{');
+
+		if (indexTesting == -1 && indexBrace != -1) {
+			LOGGER.warn("This function name is not correct: " + s);
+			return null;
+		}
+		int indexParan = s.indexOf("(", indexTesting);
+		if (indexParan == -1 && indexBrace != -1) {
+			LOGGER.warn("This function name is not correct: " + s);
+			return null;
+		}
+
+		return s.substring(indexTesting, indexParan);
+	}
 
 }
