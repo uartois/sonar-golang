c099e044c737d59b2c79e447a940c42de46e0b68
Reuben <reuben.thompson@gospel.tech>
2018-01-03 13:26:51 +0000

    Alter FunctionFinder for tests to use a regex

---
+++ src/test/resources/test_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+func IAmNotATest(t *testing.T) {
+	// do something
+}
+
+func TestEasyDef(t *testing.T) {
+	assert.True(t, true, "Should match")
+}
+
+func TestSpaceDef (t *testing.T) {
+	assert.True(t, true, "Should match")
+}
+
+func TestNoSpaceDef(t *testing.T){
+	assert.True(t, true, "Should match")
+}
+
+func TestTwoLines1 (t *testing.T) 
+{
+	assert.True(t, true, "Should match")
+}
+
+func 
+TestTwoLines2 (t *testing.T) {
+	assert.True(t, true, "Should match")
+}
+
+func TestNested(t *testing.T) {
+	t.Parallel()
+	t.Run("match", func(t *testing.T) {
+		assert.True(t, true, "Should match")
+	})
+	t.Run("nomatch", func(t *testing.T) {
+		assert.True(t, true, "Should match")
+	})
+}
