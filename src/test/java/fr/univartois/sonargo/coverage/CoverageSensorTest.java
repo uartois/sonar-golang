7d0b24913ad15afe750fbbe576d4f62df5227a74
Thibault Falque <thibault_falque@ens.univ-artois.fr>
2018-01-04 19:07:44 +0100

    add unit test for #53

--- src/test/java/fr/univartois/sonargo/coverage/CoverageSensorTest.java
+++ src/test/java/fr/univartois/sonargo/coverage/CoverageSensorTest.java
@@ -22,80 +22,96 @@ import fr.univartois.sonargo.TestUtils;
 import fr.univartois.sonargo.core.language.GoLanguage;
 
 public class CoverageSensorTest extends AbstractSonarTest {
-	private static final Logger LOGGER = Loggers.get(CoverageSensorTest.class);
+    private static final Logger LOGGER = Loggers.get(CoverageSensorTest.class);
 
-	@Before
-	@Override
-	public void init() {
-		init(TestUtils.getCoverageBaseDir());
+    @Before
+    @Override
+    public void init() {
+	init(TestUtils.getCoverageBaseDir());
+    }
+
+    @Test
+    public void testCreateStream() {
+	final CoverageSensor sensor = new CoverageSensor();
+	try (Stream<Path> paths = sensor.createStream(testerContext)) {
+
+	    assertNotNull(paths);
+
+	    paths.forEach((s) -> System.out.println(s));
+
+	} catch (final IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
+
+    @Test
+    public void testIgnoreFileStream() {
+	final CoverageSensor sensor = new CoverageSensor();
+	Stream<Path> paths;
+	try {
+	    paths = sensor.createStream(testerContext);
+	    paths.forEach((s) -> System.out.println(s));
+	    assertEquals(25, paths.count());
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-	@Test
-	public void testCreateStream() {
-		final CoverageSensor sensor = new CoverageSensor();
-		try (Stream<Path> paths = sensor.createStream(testerContext)) {
-			assertNotNull(paths);
-			LOGGER.info(paths.count() + "");
-			assertEquals(22, paths.count());
+    }
 
-		} catch (final IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    @Test
+    public void testExecution() {
+	final CoverageSensor sensor = new CoverageSensor();
+
+	try {
+	    BufferedReader reader = new BufferedReader(
+		    new FileReader(new File(CoverageSensor.class.getResource("/coverage/util/util.go").getFile())));
+
+	    String sCurrentLine;
+
+	    StringBuilder sb = new StringBuilder();
+	    while ((sCurrentLine = reader.readLine()) != null) {
+		sb.append(sCurrentLine + "\n");
+	    }
+
+	    testerContext.fileSystem().add(new DefaultInputFile("myProjectKey", "util/util.go")
+		    .setLanguage(GoLanguage.KEY).initMetadata(sb.toString()));
+
+	    reader = new BufferedReader(new FileReader(
+		    new File(CoverageSensor.class.getResource("/coverage/mathutil/mathutil.go").getFile())));
+
+	    sb = new StringBuilder();
+	    while ((sCurrentLine = reader.readLine()) != null) {
+		sb.append(sCurrentLine + "\n");
+	    }
+
+	    testerContext.fileSystem().add(new DefaultInputFile("myProjectKey", "mathutil/mathutil.go")
+		    .setLanguage(GoLanguage.KEY).initMetadata(sb.toString()));
+
+	    reader = new BufferedReader(
+		    new FileReader(new File(CoverageSensor.class.getResource("/coverage/pixel/pixel.go").getFile())));
+
+	    sb = new StringBuilder();
+	    while ((sCurrentLine = reader.readLine()) != null) {
+		sb.append(sCurrentLine + "\n");
+	    }
+
+	    testerContext.fileSystem().add(new DefaultInputFile("myProjectKey", "pixel/pixel.go")
+		    .setLanguage(GoLanguage.KEY).initMetadata(sb.toString()));
+
+	    sensor.execute(testerContext);
+
+	    LOGGER.info(testerContext.measures("myProjectKey").toString());
+
+	} catch (final FileNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (final IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-	@Test
-	public void testExecution() {
-		final CoverageSensor sensor = new CoverageSensor();
-
-		try {
-			BufferedReader reader = new BufferedReader(
-					new FileReader(new File(CoverageSensor.class.getResource("/coverage/util/util.go").getFile())));
-
-			String sCurrentLine;
-
-			StringBuilder sb = new StringBuilder();
-			while ((sCurrentLine = reader.readLine()) != null) {
-				sb.append(sCurrentLine + "\n");
-			}
-
-			testerContext.fileSystem().add(new DefaultInputFile("myProjectKey", "util/util.go")
-					.setLanguage(GoLanguage.KEY).initMetadata(sb.toString()));
-
-			reader = new BufferedReader(new FileReader(
-					new File(CoverageSensor.class.getResource("/coverage/mathutil/mathutil.go").getFile())));
-
-			sb = new StringBuilder();
-			while ((sCurrentLine = reader.readLine()) != null) {
-				sb.append(sCurrentLine + "\n");
-			}
-
-			testerContext.fileSystem().add(new DefaultInputFile("myProjectKey", "mathutil/mathutil.go")
-					.setLanguage(GoLanguage.KEY).initMetadata(sb.toString()));
-
-			reader = new BufferedReader(
-					new FileReader(new File(CoverageSensor.class.getResource("/coverage/pixel/pixel.go").getFile())));
-
-			sb = new StringBuilder();
-			while ((sCurrentLine = reader.readLine()) != null) {
-				sb.append(sCurrentLine + "\n");
-			}
-
-			testerContext.fileSystem().add(new DefaultInputFile("myProjectKey", "pixel/pixel.go")
-					.setLanguage(GoLanguage.KEY).initMetadata(sb.toString()));
-
-			sensor.execute(testerContext);
-
-			LOGGER.info(testerContext.measures("myProjectKey").toString());
-
-		} catch (final FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (final IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
+    }
 
 }
