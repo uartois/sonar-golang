52bd08fa9d62f80e8701b596e87c7838524a5986
Thibault Falque <thibault_falque@ens.univ-artois.fr>
2018-02-09 19:56:09 +0100

    add new go file for test unit #60

---
+++ src/test/resources/coverage/mathutil/a.go
@@ -0,0 +1,10 @@
+package main
+
+import (	
+	"fmt"
+)
+
+func main() {
+	foo()
+	fmt.Println("vim-go")
+}
--- src/test/resources/coverage/mathutil/filewithnocoverage.go
+++ src/test/resources/coverage/mathutil/filewithnocoverage.go
@@ -1,5 +1,13 @@
 package mathutil
 
+
+import "math"
+
+
 func function(){
 	return 1;
+}
+
+func SqrtUint32(x uint32) uint32 {
+	return uint32(math.Ceil(math.Sqrt(float64(x))))
 }
