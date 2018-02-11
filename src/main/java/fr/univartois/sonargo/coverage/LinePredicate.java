d3889bf07e1c52c1b0cf23e7a5aef73aa4ed74fb
Thibault Falque <thibault_falque@ens.univ-artois.fr>
2018-02-11 11:05:26 +0100

    comment, multiline comment, interface, struct, type interface and type struct #61

--- src/main/java/fr/univartois/sonargo/coverage/LinePredicate.java
+++ src/main/java/fr/univartois/sonargo/coverage/LinePredicate.java
@@ -2,9 +2,13 @@ package fr.univartois.sonargo.coverage;
 
 public class LinePredicate {
     private static boolean funcFound = false;
+    private static boolean commentFound = false;
+    private static boolean typeFound = false;
 
     public static void init() {
 	funcFound = false;
+	commentFound = false;
+	typeFound = false;
     }
 
     public static boolean filterLine(LineCoverage l) {
@@ -13,7 +17,31 @@ public class LinePredicate {
 	    funcFound = true;
 	    return false;
 	}
-	return funcFound && !s.isEmpty() && !"}".equals(s) && !s.matches("^type.*") && !s.matches("^func.*")
-		&& !s.matches("^import.*") && !s.matches("^package.*");
+	if (s.matches("^//.*")) {
+	    return false;
+	}
+
+	if (s.matches("^/\\*.*")) {
+	    commentFound = true;
+	    return false;
+	}
+
+	if (s.matches("^\\*/.*")) {
+	    commentFound = false;
+	    return false;
+	}
+
+	if (s.matches("^type.*") || s.matches("^interface.*") || s.matches("^struct.*")) {
+	    typeFound = true;
+	    return false;
+	}
+
+	if (s.matches("}.*") && typeFound) {
+	    typeFound = true;
+	    return false;
+	}
+
+	return funcFound && !commentFound && !typeFound && !s.isEmpty() && !"}".equals(s) && !s.matches("^type.*")
+		&& !s.matches("^func.*") && !s.matches("^import.*") && !s.matches("^package.*");
     }
 }
